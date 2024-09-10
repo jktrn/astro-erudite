ea68d4f02f0e42f3af49e2c8b3e990db34203495
enscribe <jason@enscribe.dev>
2024-09-10 14:24:09 -0700

    chore: cleanup, shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss'
 import defaultTheme from 'tailwindcss/defaultTheme'
 
 const config: Config = {
-  darkMode: ['class', 'class'],
+  darkMode: ['selector'],
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
