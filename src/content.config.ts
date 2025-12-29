5574c21890dcf5c0f6b72f57ee3a4f4b216db4c5
Ten <tencurse@gmail.com>
2025-12-29 14:10:01 -0800

    chore: clean up commented code

--- src/content.config.ts
+++ src/content.config.ts
@@ -33,20 +33,6 @@ const authors = defineCollection({
   }),
 })
 
-// const projects = defineCollection({
-//   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/projects' }),
-//   schema: ({ image }) =>
-//     z.object({
-//       name: z.string(),
-//       description: z.string(),
-//       tags: z.array(z.string()),
-//       image: image(),
-//       link: z.string().url(),
-//       startDate: z.coerce.date().optional(),
-//       endDate: z.coerce.date().optional(),
-//     }),
-// })
-
 const works = defineCollection({
   loader: glob({ pattern: '**/*.{md,mdx}', base: './src/content/works' }),
   schema: ({ image }) => 
