da498055f51e8c08df70a911c636947b0caaca61
enscribe <jason@enscribe.dev>
2026-06-24 23:59:35 -0400

    fix: `compressHTML` for whitespace

--- astro.config.ts
+++ astro.config.ts
@@ -13,6 +13,7 @@ import { headingAnchors } from "./src/lib/heading-anchors"
 
 export default defineConfig({
   site: "https://astro-erudite.vercel.app",
+  compressHTML: true,
   prefetch: { prefetchAll: true },
   integrations: [
     sitemap({
